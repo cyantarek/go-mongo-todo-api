be001dcfed3f7f103439e3da56fe63c693cd9463
dev <cyantarek.cg@gmail.com>
2018-08-13 19:40:04 +0600

    Updated

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export CGO_ENABLED=0
-cd ./src
-go build -a -installsuffix cgo -ldflags '-s' -o ../bin/crud-api
-cd ..
-chmod +x ./bin/crud-api
-docker build -t cyantarek/go-mongo-todo-api .
-docker run -d -p 8078:8078 cyantarek/go-mongo-todo-api
